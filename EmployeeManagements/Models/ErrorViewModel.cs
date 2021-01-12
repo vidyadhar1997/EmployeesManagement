5ed0a8aa146d9d245ae81885de309a733b732d18
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2021-01-12 22:51:28 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ EmployeeManagements/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace EmployeeManagements.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
